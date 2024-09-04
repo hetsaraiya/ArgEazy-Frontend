680ef71e1c95874e13dca575665fdf6c14b32066
hetsaraiya <hetsaraiya06@gmail.com>
2024-09-04 17:08:09 +0530

    :refactor: update app name, launcher icon, and package name
    - Changed app name to 'your_new_app_name' in pubspec.yaml
    - Added flutter_launcher_icons dependency for updating launcher icons
    - Configured flutter_launcher_icons with new icon path
    - Added change_app_package_name dependency for updating package name
    - Updated dev_dependencies with necessary packages

--- android/app/src/main/kotlin/com/example/argeazy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.argeazy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/hetsariya/agreazy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hetsariya.agreazy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
